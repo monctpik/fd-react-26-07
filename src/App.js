b6ccb562f12cbe5ba404e34aa9ad361e5d978905
Vladislav_Moruga <Monctpik123@gmail.com>
2023-07-27 20:55:20 +0300

    sort by lName

--- src/App.js
+++ src/App.js
@@ -10,15 +10,27 @@ class App extends Component {
     super(props);
     this.state = {
       users: [
-        { id: 3, fName: "Elon", lName: "Musk" },
-        { id: 1, fName: "Anna", lName: "Polushko" },
-        { id: 4, fName: "Vladislav", lName: "Moruga" },
-        { id: 2, fName: "Kostya", lName: "Reks" },
+        { id: 3, fName: "Elon", lName: "Ausk" },
+        { id: 1, fName: "Anna", lName: "Bolushko" },
+        { id: 4, fName: "Vladislav", lName: "Coruga" },
+        { id: 2, fName: "Kostya", lName: "Deks" },
         { id: 5, fName: "Valentin", lName: "Grigorenko" },
       ],
       isAscendingSortById: true,
+      // upDown : true,
     };
   }
+  sortByLName = () => {
+    const { users, upDown } = this.state;
+    const copyU = JSON.parse(JSON.stringify(users));
+    copyU.sort((a, b) => {
+      return upDown ? a.lName.localeCompare(b.lName) : b.lName.localeCompare(a.lName);
+    });
+    this.setState({
+      users: copyU,
+      upDown : !upDown
+    });
+  };
   sortById = () => {
     const { users, isAscendingSortById } = this.state;
     const copyUsers = JSON.parse(JSON.stringify(users));
@@ -31,7 +43,7 @@ class App extends Component {
     });
   };
   render() {
-    const { users, isAscendingSortById } = this.state;
+    const { users, isAscendingSortById, upDown } = this.state;
     return (
       <>
         <h1 className="heading" title="react">
@@ -39,6 +51,9 @@ class App extends Component {
         </h1>
         <button onClick={this.sortById}>
           Sort by ID {isAscendingSortById ? "DOWN" : "UP"}
+        </button>
+        <button onClick={this.sortByLName}>
+          Sort by LNAME {upDown ? "DOWN" : "UP"}
         </button>
         <ol>
           {users.map((user) => (
